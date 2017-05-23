ba9d2af3dcb2ae89fec0be4fc6cdfb0ea81c37c2
DQing <15829258737@163.com>
2017-05-23 14:12:57 +0800

    learn and add other method

--- test4/demo1.js
+++ test4/demo1.js
@@ -1,4 +1,6 @@
 var buf = new Buffer(256);
-var len=buf.write('jjjjjjjjjjj');
+var len=buf.write('hello my name is douqing');
 console.log(len);
 console.log(buf);
+console.log(buf.toString('utf-8'));
+console.log(buf.toString('utf-8',0,5));
---
+++ test4/demo2.js
@@ -0,0 +1,8 @@
+/*
+* 将字母转换为对应的ASCLL码
+* */
+
+buf = new Buffer("hello world");
+console.log(buf.toJSON()); //
+// { type: 'Buffer',
+//data: [ 104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100 ] }
